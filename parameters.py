342a4143c40eabcb073988df5e1fa8a84b110d92
vango <viktorlilienberg@gmail.com>
2025-12-08 21:31:14 +0100

    changed paramss

--- parameters.py
+++ parameters.py
@@ -1,7 +1,6 @@
 Tb = 0.04             # s, tid per bit
 Ac = 2.0              # carrier amplitud
 fc = 1800.0           # Hz, carrier frequens (1725+1875)/2
-# Sätt till den faktiska hårdvarusamplingsfrekvensen. Nu 45000 Hz.
 fs = 45000.0          # Hz, samplingsfrekvens
 dt = 1.0 / fs         # s, tidssteg
 channel_id = 5        # band id, använder 1725–1875 Hz i wcslib.channels
